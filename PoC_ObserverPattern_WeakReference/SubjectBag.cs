1a5de76a1c1fb3e13f275bb752bacb011e5db600
Robert <robert.avellaneda_pajares@contractors.roche.com>
2020-03-15 21:08:06 +0100

    Replace object reference by weak reference

--- PoC_ObserverPattern_WeakReference/SubjectBag.cs
+++ PoC_ObserverPattern_WeakReference/SubjectBag.cs
@@ -1,20 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
-using System.Runtime.CompilerServices;
+using System.Linq;
 
 namespace PoC_ObserverPattern_WeakReference
 {
     public class SubjectBag<T> where T : Observable<T>
     {
         private static SubjectBag<T> instance;
-        private List<Observer> observers = new List<Observer>();
+        private List<KeyValuePair<Type, WeakReference<Observer>>> observers = new List<KeyValuePair<Type, WeakReference<Observer>>>();
+        private readonly WeakReference<T> observable;
 
         internal SubjectBag(T observable)
         {
-            this.Observable = observable;
+            this.observable = new WeakReference<T>(observable);
         }
 
-        public readonly T Observable;
+        public T Observable => this.observable.TryGetTarget(out var target) ? target : null;
 
         public static SubjectBag<T> Instance(T observable)
         {
@@ -33,19 +35,29 @@ namespace PoC_ObserverPattern_WeakReference
 
         public void Attach(Observer observer)
         {
-            this.observers.Add(observer);
+            this.observers.Add(
+                new KeyValuePair<Type, WeakReference<Observer>>(
+                    observer.GetType(), 
+                    new WeakReference<Observer>(observer)));
         }
 
         public void Detach(Observer observer)
         {
-            this.observers.Remove(observer);
+            if (this.observers.Any(x => x.Key == observer.GetType()))
+            {
+                var elementToRemove = this.observers.FirstOrDefault(x => x.Key == observer.GetType());
+                this.observers.Remove(elementToRemove);
+            }
         }
 
         public void Notify()
         {
             foreach (var observer in this.observers)
             {
-                observer.Update();
+                if (observer.Value.TryGetTarget(out var observerToNotify))
+                {
+                    observerToNotify.Update();
+                }
             }
         }
     }
